35bb0995ee69e3109583e2678e4dc703b4066cc5
Elaine Yu <elaineicecream@Elaines-MacBook-Pro.local>
2014-02-05 13:24:59 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapboxTestExample::Application.config.secret_key_base = '4f506b031b615a79c4655c787a095cf300553612f5cf2c5378171aaca66329659946eb4cda05168c18ed9be3db051da01a9359bfe28e14851985bac701342a34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MapboxTestExample::Application.config.session_store :cookie_store, key: '_mapbox_test_example_session'
